90bbb0a1710d388bf67041cf7281c29798624ac2
Brian Fitzgerald <cooldude242@gmail.com>
2016-02-25 20:41:01 -0600

    rm config

--- chaperone.js
+++ chaperone.js
@@ -3,6 +3,8 @@ var express = require('express');
 var program = require('commander');
 var github = require('octonode');
 var fs = require('fs');
+var Botkit = require('Botkit');
+var os = require('os');
 
 program
 	.version('0.0.1')
@@ -14,6 +16,120 @@ program
 
 
 var params = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+
+/*
+	Slack bot integration
+*/
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: params.token
+}).startRTM();
+
+
+controller.hears(['invite me (.*)', 'sign me up (.*)', 'invite (.*)'], 'direct_message', function (bot, message) {
+	var matches = message.text.match(/call me (.*)/i);
+	var username = matches[1];
+
+	org.addMember(username, {}, function (err, data, headers) {
+		if (err) console.log(err);
+		if (data) {
+			console.log(data);
+			bot.reply('You\'re in! Now, check out our projects here: https://github.com/mizzou-hackers/');
+		}
+	});
+
+});
+
+controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
+    controller.storage.users.get(message.user,function(err, user) {
+        if (user && user.name) {
+            bot.reply(message,'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message,'Hello.');
+        }
+    });
+});
+
+controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+    var matches = message.text.match(/call me (.*)/i);
+    var name = matches[1];
+    controller.storage.users.get(message.user,function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user,function(err, id) {
+            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    bot.startConversation(message,function(err, convo) {
+
+        convo.ask('Are you sure you want me to shutdown?',[
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    },3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
+
+
+controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    var hostname = os.hostname();
+    var uptime = formatUptime(process.uptime());
+
+    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+});
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
+
+
+/*
+	Github integration
+*/
 
 var client = github.client({
 	username: params.username,
